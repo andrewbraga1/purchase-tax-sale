04b68d1409c767a60f8076465d4a7355ce307f41
Andrew Braga <andrewbraga1@gmail.com>
2022-11-18 14:24:31 -0300

    feat: builded sanitizer to Do: - initialize product - create calculations for product

--- lib/models/purchase.rb
+++ lib/models/purchase.rb
@@ -1,6 +1,8 @@
 require_relative 'product'
+require './lib/helpers/sanitize_order_helper'
 require './lib/constants_v1'
 class Purchase 
+    include SanitizeOrderHelper
     attr_accessor :products
 
     def initialize(file, product_generator = Product)
@@ -8,6 +10,7 @@ class Purchase
         @generator = product_generator
         @products = []
         @total, @total_taxes = ZERO_FLOAT, ZERO_FLOAT
+        process
     end 
 
     def process
@@ -18,8 +21,11 @@ class Purchase
     
     def load_file file
         File.foreach(file) do |order| 
-           # receive file data which every line it is an order(items purchase)
-           # create helper for 'sanitization'
+            quantity = get_quantity order
+            price = get_price order
+            is_imported = is_imported? order
+            name = get_product_name(order)
+            p quantity,name, price, is_imported 
         end
     end
  
