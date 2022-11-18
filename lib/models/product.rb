7b72c0de08b80a7f7238a5ad0f18ae17f5fe7de3
Andrew Braga <andrewbraga1@gmail.com>
2022-11-18 13:24:31 -0300

    feat: pass last case test - case upper => success!

--- lib/models/product.rb
+++ lib/models/product.rb
@@ -1,5 +1,16 @@
 class Product
+    def accuracy
+        1/0.05
+    end
+
     def round_tax_rule amount 
-        ((amount * 1/0.05).round/(1/0.05))
+        result_rounded_numerator = ((amount * accuracy).round/accuracy) 
+        true_result = ((amount * accuracy)/accuracy) 
+        if true_result > result_rounded_numerator
+            result = result_rounded_numerator + 0.05
+        else
+            result = result_rounded_numerator
+        end
+        return result.round(2)
     end
 end
